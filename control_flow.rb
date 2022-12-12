c2b33496c6693a9d8bf03d9b9960e6a89150387a
Kamran Hussein <chinterswill@gmail.com>
2022-12-12 16:44:52 -0500

    commit tree f2626eb5649ad9f5f21ac6a4948f1a437c7f9918

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,58 @@
 def admin_login(username, password)
-  # your code here
+  if username == 'admin' && password == '12345'
+    puts 'Access granted'
+  elsif username == 'ADMIN' && password == '12345'
+    puts 'Access granted'
+  else 
+    puts 'Access denied'
+  end
 end
 
+admin_login('admin', '12345')
+admin_login('ADMIN', '12345')
+admin_login('sudo', '12345')
+
+
+
+
+
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    puts `It's brisk out there!`
+  elsif temperature <= 65
+    puts `It's a little chilly out there!`
+  elsif temperature > 85
+    puts `It's too dang hot out there!`
+  else
+    return `It's perfect out there!`
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  num1 = num / 3
+  num2 = num / 5
+  if class.num1 = Integer
+    puts 'Fizz'
+  elsif class.num2 = Integer
+    puts 'Buzz'
+  elsif class.num1 && class.num2 = Integer
+    puts 'FizzBuzz'
+  else
+    return num
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when '+'
+    puts num1 + num2
+  when '-'
+    puts num1 - num2
+  when '*'
+    puts num1 * num2
+  when '/'
+    puts num1 / num2
+  else 
+    'Invalid Operation!'
+  end
 end
 
